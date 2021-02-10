cc299db601dee75b2a980eb374d3c9de470b0c77
Brittany Lindberg <blindberg29@gmail.com>
2021-02-09 17:48:27 -0800

    commit

--- js/ex4.js
+++ js/ex4.js
@@ -1,16 +1,18 @@
-public static void main(String[] args) 
-{
-    final String PASSWORD = "secret";
-    Scanner sc = new Scanner(System.in);
-    int attempts = 3;
-    String password = "";
-    while (attempts-- > 0 && !PASSWORD.equals(password)) //compares and then decrements
-    {
-        System.out.print("Enter your password: ");
-        password = sc.nextLine();
-        if (password.equals(PASSWORD)) 
-            System.out.println("You entered the correct password");
-        else 
-            System.out.println(`Your account is locked! You failed to enter the correct password # times: ${attempts}`);        
+var EnteredPassword = prompt("Password: ")
+var count = 0 
+
+while (count <= 2) {
+    if (EnteredPassword != "secret")  {
+        alert("Incorrect password ! Please enter again")
+        var EnteredPassword = prompt("Password: ")
     }
+    else {
+        alert(`You entered the correct password after ${count} attempt(s)`)
+        break
+    }
+    count++;
+}
+
+if (count > 2 ) {
+    alert(`account is locked!You failed to enter the correct password ${count} times`)
 }
